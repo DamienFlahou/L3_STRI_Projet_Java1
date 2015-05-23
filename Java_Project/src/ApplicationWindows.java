6a3f53c8c853a5945c5466dad4402fb286ebafae
paul-sfeir <paul.sfeir@gmail.com>
2015-05-23 21:05:25 +0200

    Nettoyage du git

--- Java_Project/src/ApplicationWindows.java
+++ Java_Project/src/ApplicationWindows.java
@@ -59,30 +59,30 @@ public class ApplicationWindows {
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		frame.getContentPane().add(tabbedPane, BorderLayout.CENTER);
 		
-		JPanel Réseau_physique = new JPanel();
-		tabbedPane.addTab("R\u00E9seau physique", null, Réseau_physique, null);
-		Réseau_physique.setLayout(null);
+		JPanel Reseau_physique = new JPanel();
+		tabbedPane.addTab("R\u00E9seau physique", null, Reseau_physique, null);
+		Reseau_physique.setLayout(null);
 		
-		JLabel lblCartesRéseauxP = new JLabel("Cartes r\u00E9seaux");
-		lblCartesRéseauxP.setBorder(new LineBorder(new Color(0, 0, 0)));
-		lblCartesRéseauxP.setForeground(Color.BLACK);
-		lblCartesRéseauxP.setHorizontalAlignment(SwingConstants.CENTER);
-		lblCartesRéseauxP.setBounds(450, 33, 135, 23);
-		Réseau_physique.add(lblCartesRéseauxP);
+		JLabel lblCartesReseauxP = new JLabel("Cartes r\u00E9seaux");
+		lblCartesReseauxP.setBorder(new LineBorder(new Color(0, 0, 0)));
+		lblCartesReseauxP.setForeground(Color.BLACK);
+		lblCartesReseauxP.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCartesReseauxP.setBounds(450, 33, 135, 23);
+		Reseau_physique.add(lblCartesReseauxP);
 		
 		JLabel lblOrdinateursP = new JLabel("Ordinateurs");
 		lblOrdinateursP.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblOrdinateursP.setForeground(Color.BLACK);
 		lblOrdinateursP.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOrdinateursP.setBounds(316, 33, 135, 23);
-		Réseau_physique.add(lblOrdinateursP);
+		Reseau_physique.add(lblOrdinateursP);
 		
 		JLabel lblSalles = new JLabel("Salles");
 		lblSalles.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblSalles.setForeground(Color.BLACK);
 		lblSalles.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSalles.setBounds(182, 33, 135, 23);
-		Réseau_physique.add(lblSalles);
+		Reseau_physique.add(lblSalles);
 		
 		JLabel lblLocaux = new JLabel("Locaux");
 		lblLocaux.setBorder(new LineBorder(new Color(0, 0, 0)));
@@ -90,28 +90,28 @@ public class ApplicationWindows {
 		lblLocaux.setBackground(Color.WHITE);
 		lblLocaux.setHorizontalAlignment(SwingConstants.CENTER);
 		lblLocaux.setBounds(48, 33, 135, 23);
-		Réseau_physique.add(lblLocaux);
+		Reseau_physique.add(lblLocaux);
 		
 		JList list_locaux = new JList();
 		list_locaux.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_locaux.setToolTipText("");
 		list_locaux.setBounds(48, 54, 135, 157);
-		Réseau_physique.add(list_locaux);
+		Reseau_physique.add(list_locaux);
 		
 		JList list_salles = new JList();
 		list_salles.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_salles.setBounds(182, 54, 135, 157);
-		Réseau_physique.add(list_salles);
+		Reseau_physique.add(list_salles);
 		
 		JList list_ordinateurs_physique = new JList();
 		list_ordinateurs_physique.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_ordinateurs_physique.setBounds(316, 54, 135, 157);
-		Réseau_physique.add(list_ordinateurs_physique);
+		Reseau_physique.add(list_ordinateurs_physique);
 		
-		JList list_cartes_réseaux = new JList();
-		list_cartes_réseaux.setBorder(new LineBorder(new Color(0, 0, 0)));
-		list_cartes_réseaux.setBounds(450, 54, 135, 157);
-		Réseau_physique.add(list_cartes_réseaux);
+		JList list_cartes_reseaux = new JList();
+		list_cartes_reseaux.setBorder(new LineBorder(new Color(0, 0, 0)));
+		list_cartes_reseaux.setBounds(450, 54, 135, 157);
+		Reseau_physique.add(list_cartes_reseaux);
 		
 		JButton btnAjouter = new JButton("Ajouter");
 		btnAjouter.setFont(new Font("Tahoma", Font.PLAIN, 10));
@@ -120,113 +120,113 @@ public class ApplicationWindows {
 			}
 		});
 		btnAjouter.setBounds(30, 222, 89, 23);
-		Réseau_physique.add(btnAjouter);
+		Reseau_physique.add(btnAjouter);
 		
 		JButton btnSupprimer = new JButton("Supprimer");
 		btnSupprimer.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnSupprimer.setBounds(228, 222, 89, 23);
-		Réseau_physique.add(btnSupprimer);
+		Reseau_physique.add(btnSupprimer);
 		
 		JButton btnMiseJour = new JButton("Mise \u00E0 jour");
 		btnMiseJour.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnMiseJour.setBounds(327, 222, 89, 23);
-		Réseau_physique.add(btnMiseJour);
+		Reseau_physique.add(btnMiseJour);
 		
 		JButton btnActiver = new JButton("Activer");
 		btnActiver.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnActiver.setBounds(426, 222, 89, 23);
-		Réseau_physique.add(btnActiver);
+		Reseau_physique.add(btnActiver);
 		
 		JButton btnDsactiver = new JButton("D\u00E9sactiver");
 		btnDsactiver.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnDsactiver.setBounds(525, 222, 89, 23);
-		Réseau_physique.add(btnDsactiver);
+		Reseau_physique.add(btnDsactiver);
 		
 		JButton btnModifier = new JButton("Modifier");
 		btnModifier.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnModifier.setBounds(129, 222, 89, 23);
-		Réseau_physique.add(btnModifier);
+		Reseau_physique.add(btnModifier);
 		
-		JPanel Réseau_logique = new JPanel();
-		tabbedPane.addTab("R\u00E9seau logique", null, Réseau_logique, null);
-		Réseau_logique.setLayout(null);
+		JPanel Reseau_logique = new JPanel();
+		tabbedPane.addTab("R\u00E9seau logique", null, Reseau_logique, null);
+		Reseau_logique.setLayout(null);
 		
 		JLabel lblCartesReseauxL = new JLabel("Cartes r\u00E9seaux");
 		lblCartesReseauxL.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblCartesReseauxL.setForeground(Color.BLACK);
 		lblCartesReseauxL.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCartesReseauxL.setBounds(450, 33, 135, 23);
-		Réseau_logique.add(lblCartesReseauxL);
+		Reseau_logique.add(lblCartesReseauxL);
 		
 		JLabel lblOrdinateursL = new JLabel("Ordinateurs");
 		lblOrdinateursL.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblOrdinateursL.setHorizontalAlignment(SwingConstants.CENTER);
 		lblOrdinateursL.setForeground(Color.BLACK);
 		lblOrdinateursL.setBounds(316, 33, 135, 23);
-		Réseau_logique.add(lblOrdinateursL);
+		Reseau_logique.add(lblOrdinateursL);
 		
 		JLabel lblNewLabel = new JLabel("Switchs");
 		lblNewLabel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblNewLabel.setForeground(Color.BLACK);
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setBounds(182, 33, 135, 23);
-		Réseau_logique.add(lblNewLabel);
+		Reseau_logique.add(lblNewLabel);
 		
 		JLabel lblRouteurs = new JLabel("Routeurs");
 		lblRouteurs.setBorder(new LineBorder(new Color(0, 0, 0)));
 		lblRouteurs.setForeground(Color.BLACK);
 		lblRouteurs.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRouteurs.setBounds(48, 33, 135, 23);
-		Réseau_logique.add(lblRouteurs);
+		Reseau_logique.add(lblRouteurs);
 		
 		JList list_routeurs = new JList();
 		list_routeurs.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_routeurs.setBounds(48, 54, 135, 157);
-		Réseau_logique.add(list_routeurs);
+		Reseau_logique.add(list_routeurs);
 		
 		JList list_switchs = new JList();
 		list_switchs.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_switchs.setBounds(182, 54, 135, 157);
-		Réseau_logique.add(list_switchs);
+		Reseau_logique.add(list_switchs);
 		
 		JList list_ordinateurs_logique = new JList();
 		list_ordinateurs_logique.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_ordinateurs_logique.setBounds(316, 54, 135, 157);
-		Réseau_logique.add(list_ordinateurs_logique);
+		Reseau_logique.add(list_ordinateurs_logique);
 		
-		JList list_cartes_réseaux_logique = new JList();
-		list_cartes_réseaux_logique.setBorder(new LineBorder(new Color(0, 0, 0)));
-		list_cartes_réseaux_logique.setBounds(450, 54, 135, 157);
-		Réseau_logique.add(list_cartes_réseaux_logique);
+		JList list_cartes_reseaux_logique = new JList();
+		list_cartes_reseaux_logique.setBorder(new LineBorder(new Color(0, 0, 0)));
+		list_cartes_reseaux_logique.setBounds(450, 54, 135, 157);
+		Reseau_logique.add(list_cartes_reseaux_logique);
 		
 		JButton btnAjouter_1 = new JButton("Ajouter");
 		btnAjouter_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnAjouter_1.setBounds(30, 222, 89, 23);
-		Réseau_logique.add(btnAjouter_1);
+		Reseau_logique.add(btnAjouter_1);
 		
 		JButton btnModifier_1 = new JButton("Modifier");
 		btnModifier_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnModifier_1.setBounds(129, 222, 89, 23);
-		Réseau_logique.add(btnModifier_1);
+		Reseau_logique.add(btnModifier_1);
 		
 		JButton btnSupprimer_1 = new JButton("Supprimer");
 		btnSupprimer_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnSupprimer_1.setBounds(228, 222, 89, 23);
-		Réseau_logique.add(btnSupprimer_1);
+		Reseau_logique.add(btnSupprimer_1);
 		
 		JButton btnMiseJour_1 = new JButton("Mise \u00E0 jour");
 		btnMiseJour_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnMiseJour_1.setBounds(327, 222, 89, 23);
-		Réseau_logique.add(btnMiseJour_1);
+		Reseau_logique.add(btnMiseJour_1);
 		
 		JButton btnActiver_1 = new JButton("Activer");
 		btnActiver_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnActiver_1.setBounds(426, 222, 89, 23);
-		Réseau_logique.add(btnActiver_1);
+		Reseau_logique.add(btnActiver_1);
 		
 		JButton btnDsactiver_1 = new JButton("D\u00E9sactiver");
 		btnDsactiver_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnDsactiver_1.setBounds(525, 222, 89, 23);
-		Réseau_logique.add(btnDsactiver_1);
+		Reseau_logique.add(btnDsactiver_1);
 	}
 }
