b73556f9673d19961e03d862dc21953f83711465
paul-sfeir <paul.sfeir@gmail.com>
2015-05-25 03:16:07 +0200

    Gestion de clique sur les différentes liste: - modification du texte des boutons - enlever l'indice de la liste précédemment sélectionnée

--- Java_Project/src/ApplicationWindows.java
+++ Java_Project/src/ApplicationWindows.java
@@ -1,5 +1,3 @@
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JTabbedPane;
 
@@ -9,25 +7,192 @@ import javax.swing.JPanel;
 import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.ListSelectionModel;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import java.awt.Color;
 
 import javax.swing.SwingConstants;
 
 import java.awt.Font;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.ArrayList;
 
 
-public class ApplicationWindows extends JFrame{
+public class ApplicationWindows extends JFrame implements ListSelectionListener, MouseListener{
 	
+	public static final int SLocal = 1;
+	public static final int SSalle = 2;
+	public static final int SOrdinateurPhysique = 3;
+	public static final int SCarteReseauPhysique = 4;
+	public static final int SRouteur = 5;
+	public static final int SSwitch = 6;
+	public static final int SOrdinateurLogique = 7;
+	public static final int SCarteReseauLogique = 8;
+	
+	// Sert à savoir quelle liste est focus pour pouvoir adapter le comportement des boutons
+	private int focusList = 1;
+	
+	public int getFocusList() {
+		return focusList;
+	}
+
+	public void setFocusList(int focusList) {
+		this.focusList = focusList;
+	}
+
+	ArrayList<Local> listeLocaux = new ArrayList<Local>();
+	ArrayList<Salle> listeSalles = new ArrayList<Salle>();
+	ArrayList<Ordinateur> listeOrdinateurs = new ArrayList<Ordinateur>();
+	ArrayList<CarteReseau> listeCarteReseaux = new ArrayList<CarteReseau>();
+	
+	ArrayList<Routeur> listeRouteurs = new ArrayList<Routeur>();
+	ArrayList<Switch> listeSwitchs = new ArrayList<Switch>();
+	ArrayList<Ordinateur> listeOrdinateurs2 = new ArrayList<Ordinateur>();
+	ArrayList<CarteReseau> listeCarteReseaux2 = new ArrayList<CarteReseau>();
+	
+	private JList list_locaux;
+	private JList list_salles;
+	private JList list_ordinateurs_physique;
+	private JList list_cartes_reseaux;
+	private JList list_routeurs;
+	private JList list_switchs;
+	private JList list_ordinateurs_logique;
+	private JList list_cartes_reseaux_logique;
+	
+	private JButton btnAjouter;
+	private JButton btnSupprimer;
+	private JButton btnModifier;
+	private JButton btnMiseJour;
+	private JButton btnActiver;
+	private JButton btnDsactiver;
+	
+	private JButton btnAjouter_1;
+	private JButton btnSupprimer_1;
+	private JButton btnModifier_1;
+	private JButton btnMiseJour_1;
+	private JButton btnActiver_1;
+	private JButton btnDsactiver_1;
+	
+	
+	
+	public JButton getBtnSupprimer() {
+		return btnSupprimer;
+	}
+
+	public JButton getBtnModifier() {
+		return btnModifier;
+	}
+
+	public JButton getBtnMiseJour() {
+		return btnMiseJour;
+	}
+
+	public JButton getBtnActiver() {
+		return btnActiver;
+	}
+
+	public JButton getBtnDsactiver() {
+		return btnDsactiver;
+	}
+
+	public JButton getBtnAjouter_1() {
+		return btnAjouter_1;
+	}
+
+	public JButton getBtnSupprimer_1() {
+		return btnSupprimer_1;
+	}
+
+	public JButton getBtnModifier_1() {
+		return btnModifier_1;
+	}
+
+	public JButton getBtnMiseJour_1() {
+		return btnMiseJour_1;
+	}
+
+	public JButton getBtnActiver_1() {
+		return btnActiver_1;
+	}
+
+	public JButton getBtnDsactiver_1() {
+		return btnDsactiver_1;
+	}
+
+	public JButton getBtnAjouter() {
+		return btnAjouter;
+	}
+
+	public JList getList_locaux() {
+		return list_locaux;
+	}
+
+	public JList getList_salles() {
+		return list_salles;
+	}
+
+	public JList getList_ordinateurs_physique() {
+		return list_ordinateurs_physique;
+	}
+
+	public JList getList_cartes_reseaux() {
+		return list_cartes_reseaux;
+	}
+
+	public JList getList_routeurs() {
+		return list_routeurs;
+	}
+
+	public JList getList_switchs() {
+		return list_switchs;
+	}
+
+	public JList getList_ordinateurs_logique() {
+		return list_ordinateurs_logique;
+	}
+
+	public JList getList_cartes_reseaux_logique() {
+		return list_cartes_reseaux_logique;
+	}
+
 	public ApplicationWindows() {
+
+		listeLocaux.add(new Local("local 1"));
+		listeLocaux.add(new Local("local 2"));
+		
+		listeSalles.add(new Salle("salle 1"));
+		listeSalles.add(new Salle("salle 2"));
+		
+		listeOrdinateurs.add(new Ordinateur("ordinateur 1"));
+		listeOrdinateurs.add(new Ordinateur("ordinateur 2"));
+		
+		listeCarteReseaux.add(new CarteReseau("00:00:00:00:00"));
+		listeCarteReseaux.add(new CarteReseau("11:11:11:11:11"));
+		
+		listeRouteurs.add(new Routeur("routeur 1"));
+		listeRouteurs.add(new Routeur("routeur 2"));
+		
+		listeSwitchs.add(new Switch("switch 1"));
+		listeSwitchs.add(new Switch("switch 2"));
+		
+		listeOrdinateurs2.add(new Ordinateur("ordinateur 3"));
+		listeOrdinateurs2.add(new Ordinateur("ordinateur 4"));
+		
+		listeCarteReseaux2.add(new CarteReseau("22:22:22:22:22"));
+		listeCarteReseaux2.add(new CarteReseau("33:33:33:33:33"));
+		
 		initialize();
+		
 	}
 
 	/**
@@ -36,6 +201,7 @@ public class ApplicationWindows extends JFrame{
 	private void initialize() {
 		this.setBounds(100, 100, 663, 336);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		this.getContentPane().add(tabbedPane, BorderLayout.CENTER);
@@ -73,60 +239,77 @@ public class ApplicationWindows extends JFrame{
 		lblLocaux.setBounds(48, 33, 135, 23);
 		Reseau_physique.add(lblLocaux);
 		
-		JList list_locaux = new JList();
+		list_locaux = new JList(listeLocaux.toArray());
 		list_locaux.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_locaux.setToolTipText("");
 		list_locaux.setBounds(48, 54, 135, 157);
 		Reseau_physique.add(list_locaux);
 		
-		JList list_salles = new JList();
+		
+		list_salles = new JList(listeSalles.toArray());
 		list_salles.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_salles.setBounds(182, 54, 135, 157);
 		Reseau_physique.add(list_salles);
 		
-		JList list_ordinateurs_physique = new JList();
+		list_ordinateurs_physique = new JList(listeOrdinateurs.toArray());
 		list_ordinateurs_physique.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_ordinateurs_physique.setBounds(316, 54, 135, 157);
 		Reseau_physique.add(list_ordinateurs_physique);
 		
-		JList list_cartes_reseaux = new JList();
+		list_cartes_reseaux = new JList(listeCarteReseaux.toArray());
 		list_cartes_reseaux.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_cartes_reseaux.setBounds(450, 54, 135, 157);
 		Reseau_physique.add(list_cartes_reseaux);
+
 		
-		JButton btnAjouter = new JButton("Ajouter");
+		//Ajoute List Selection listener sur les listes pour savoir quel type d'ajout ou de modification il faudra faire
+		list_locaux.addListSelectionListener(new ControleurSelectionListe(this, SLocal));
+		list_salles.addListSelectionListener(new ControleurSelectionListe(this, SSalle));
+		list_ordinateurs_physique.addListSelectionListener(new ControleurSelectionListe(this, SOrdinateurPhysique));
+		list_cartes_reseaux.addListSelectionListener(new ControleurSelectionListe(this, SCarteReseauPhysique));
+		
+		btnAjouter = new JButton("Ajouter");
 		btnAjouter.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnAjouter.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		btnAjouter.setBounds(30, 222, 89, 23);
+		btnAjouter.setBounds(30, 222, 89, 35);
 		Reseau_physique.add(btnAjouter);
 		
-		JButton btnSupprimer = new JButton("Supprimer");
+		btnSupprimer = new JButton("Supprimer");
 		btnSupprimer.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnSupprimer.setBounds(228, 222, 89, 23);
+		btnSupprimer.setBounds(228, 223, 89, 34);
 		Reseau_physique.add(btnSupprimer);
 		
-		JButton btnMiseJour = new JButton("Mise \u00E0 jour");
+		btnMiseJour = new JButton("Mise \u00E0 jour");
 		btnMiseJour.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnMiseJour.setBounds(327, 222, 89, 23);
+		btnMiseJour.setBounds(327, 222, 89, 35);
 		Reseau_physique.add(btnMiseJour);
 		
-		JButton btnActiver = new JButton("Activer");
+		btnActiver = new JButton("Activer");
 		btnActiver.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnActiver.setBounds(426, 222, 89, 23);
+		btnActiver.setBounds(426, 222, 89, 35);
 		Reseau_physique.add(btnActiver);
 		
-		JButton btnDsactiver = new JButton("D\u00E9sactiver");
+		btnDsactiver = new JButton("D\u00E9sactiver");
 		btnDsactiver.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnDsactiver.setBounds(525, 222, 89, 23);
+		btnDsactiver.setBounds(525, 222, 89, 35);
 		Reseau_physique.add(btnDsactiver);
 		
-		JButton btnModifier = new JButton("Modifier");
+		btnModifier = new JButton("Modifier");
 		btnModifier.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		btnModifier.setBounds(129, 222, 89, 23);
+		btnModifier.setBounds(129, 222, 89, 35);
 		Reseau_physique.add(btnModifier);
+		
+		//Ajout du mouse listener pour savoir quand le bouton est cliqué
+		
+		btnAjouter.addMouseListener(this);
+		btnActiver.addMouseListener(this);
+		btnDsactiver.addMouseListener(this);
+		btnMiseJour.addMouseListener(this);
+		btnModifier.addMouseListener(this);
+		btnSupprimer.addMouseListener(this);
 		
 		JPanel Reseau_logique = new JPanel();
 		tabbedPane.addTab("R\u00E9seau logique", null, Reseau_logique, null);
@@ -160,54 +343,106 @@ public class ApplicationWindows extends JFrame{
 		lblRouteurs.setBounds(48, 33, 135, 23);
 		Reseau_logique.add(lblRouteurs);
 		
-		JList list_routeurs = new JList();
+		list_routeurs = new JList(listeRouteurs.toArray());
 		list_routeurs.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_routeurs.setBounds(48, 54, 135, 157);
 		Reseau_logique.add(list_routeurs);
 		
-		JList list_switchs = new JList();
+		list_switchs = new JList(listeSwitchs.toArray());
 		list_switchs.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_switchs.setBounds(182, 54, 135, 157);
 		Reseau_logique.add(list_switchs);
 		
-		JList list_ordinateurs_logique = new JList();
+		list_ordinateurs_logique = new JList(listeOrdinateurs2.toArray());
 		list_ordinateurs_logique.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_ordinateurs_logique.setBounds(316, 54, 135, 157);
 		Reseau_logique.add(list_ordinateurs_logique);
 		
-		JList list_cartes_reseaux_logique = new JList();
+		list_cartes_reseaux_logique = new JList(listeCarteReseaux2.toArray());
 		list_cartes_reseaux_logique.setBorder(new LineBorder(new Color(0, 0, 0)));
 		list_cartes_reseaux_logique.setBounds(450, 54, 135, 157);
 		Reseau_logique.add(list_cartes_reseaux_logique);
 		
-		JButton btnAjouter_1 = new JButton("Ajouter");
+		//Ajoute List Selection listener sur les listes pour savoir quel type d'ajout ou de modification il faudra faire
+		list_routeurs.addListSelectionListener(new ControleurSelectionListe(this, SRouteur));
+		list_switchs.addListSelectionListener(new ControleurSelectionListe(this, SSwitch));
+		list_ordinateurs_logique.addListSelectionListener(new ControleurSelectionListe(this, SOrdinateurLogique));
+		list_cartes_reseaux_logique.addListSelectionListener(new ControleurSelectionListe(this, SCarteReseauLogique));
+		
+		btnAjouter_1 = new JButton("Ajouter");
 		btnAjouter_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnAjouter_1.setBounds(30, 222, 89, 23);
 		Reseau_logique.add(btnAjouter_1);
 		
-		JButton btnModifier_1 = new JButton("Modifier");
+		btnModifier_1 = new JButton("Modifier");
 		btnModifier_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnModifier_1.setBounds(129, 222, 89, 23);
 		Reseau_logique.add(btnModifier_1);
 		
-		JButton btnSupprimer_1 = new JButton("Supprimer");
+		btnSupprimer_1 = new JButton("Supprimer");
 		btnSupprimer_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnSupprimer_1.setBounds(228, 222, 89, 23);
 		Reseau_logique.add(btnSupprimer_1);
 		
-		JButton btnMiseJour_1 = new JButton("Mise \u00E0 jour");
+		btnMiseJour_1 = new JButton("Mise \u00E0 jour");
 		btnMiseJour_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnMiseJour_1.setBounds(327, 222, 89, 23);
 		Reseau_logique.add(btnMiseJour_1);
 		
-		JButton btnActiver_1 = new JButton("Activer");
+		btnActiver_1 = new JButton("Activer");
 		btnActiver_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnActiver_1.setBounds(426, 222, 89, 23);
 		Reseau_logique.add(btnActiver_1);
 		
-		JButton btnDsactiver_1 = new JButton("D\u00E9sactiver");
+		btnDsactiver_1 = new JButton("D\u00E9sactiver");
 		btnDsactiver_1.setFont(new Font("Tahoma", Font.PLAIN, 10));
 		btnDsactiver_1.setBounds(525, 222, 89, 23);
 		Reseau_logique.add(btnDsactiver_1);
+		
+		
 	}
+
+	public static int getScartereseauphysique() {
+		return SCarteReseauPhysique;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+		JList lsm = (JList)e.getSource();
+			System.out.println(lsm.getName());
+		
+	}
+	
+	
 }
